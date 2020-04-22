d3f87e6ec7b721d51da4e9e88e2080033192a35b
ChegeDaniella <chegedaniella@gmail.com>
2020-04-22 10:18:22 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+from .request import get_movies,get_movie
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
